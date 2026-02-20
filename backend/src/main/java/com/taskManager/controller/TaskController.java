2da2a48bebb8642f11fcd81dd72317f0766c2d12
Sanjay-R33 <sanjayr200433@gmail.com>
2026-02-20 15:26:20 +0530

    Organize backend into backend folder 3

--- backend/src/main/java/com/taskManager/controller/TaskController.java
+++ backend/src/main/java/com/taskManager/controller/TaskController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/tasks")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class TaskController {
 
     private final TaskService service;
